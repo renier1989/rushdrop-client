f5b66b502319f72ed5a709cfef0da7fd620a917d
Renier Vargas <renier1989@gmail.com>
2024-02-06 13:17:16 -0500

    aplicando cambios para poder descargar el archivo contectando con el Backend

--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -1,10 +1,42 @@
 import Layout from "@/components/Layout";
+import clienteAxios from "@/config/axios";
 import React from "react";
 
-const Enlace = () => {
+export async function getServerSideProps({ params }) {
+  // export async function getStaticProps({ params }) {
+  const { enlace } = params;
+  const respuesta = await clienteAxios.get(`/api/enlaces/${enlace}`);
+
+  return {
+    props: {
+      link: respuesta.data,
+    },
+  };
+}
+export async function getServerSidePaths() {
+  // export async function getStaticPaths() {
+  const enlaces = await clienteAxios.get("/api/enlaces");
+  return {
+    paths: enlaces.data.enlaces.map((enlace) => ({
+      params: { enlace: enlace.url },
+    })),
+    fallback: false,
+  };
+}
+
+const Enlace = ({ link }) => {
+  console.log(link);
   return (
     <Layout>
-      <div>Enlace [enlace]</div>
+      <div className="text-center flex flex-col items-center justify-center font-bold text-sky-900 gap-8">
+        <p className="text-4xl">Descarga tu archivo:</p>
+        <a 
+        href={`${process.env.backendURL}/api/archivos/${link.archivo}`} 
+        download
+        className="bg-rose-600 text-white px-4 text-2xl py-1 rounded-md uppercase font-bold hover:shadow-lg transition-all duration-300">
+          Descargar
+        </a>
+      </div>
     </Layout>
   );
 };

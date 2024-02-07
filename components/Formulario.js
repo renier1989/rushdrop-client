69fcc389af29d3d385f97cd8f9d0f59aa8597106
Renier Vargas <renier1989@gmail.com>
2024-02-07 13:55:38 -0500

    creando el formulario de opciones extras para los usuario autenticados

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 const Formulario = () => {
+  const [asignarPass, setAsignarPass] = useState(false);
+
   return (
-    <div className="w-full mt-2 text-center mx-5">
-      <div>
+    <div className="mt-2 py-5 mx-5 bg-white/70 rounded-md">
+      <div className="mx-5">
         <label
           htmlFor="descargas"
           className="text-lg text-sky-900 font-semibold"
@@ -11,14 +15,33 @@ const Formulario = () => {
         <select
           name="descargas"
           id="descargas"
-          className="appearance-none w-3/4 text-center mt-2 mb-10 bg-white border-2 font-semibold border-rose-900 text-black py3 focus:outline-none rounded-md"
+          className="appearance-none w-full text-center mt-2 bg-white border-2 font-semibold border-rose-900 text-black py3 focus:outline-none rounded-md"
         >
-          <option value="" selected disabled>-- Cantidad de descargas --</option>
+          <option value="" selected disabled>
+            -- Cantidad de descargas --
+          </option>
           <option value="1">1 Descarga</option>
           <option value="5">5 Descargas</option>
           <option value="10">10 Descargas</option>
           <option value="20">20 Descargas</option>
         </select>
+      </div>
+      <div className="mt-4 mx-5">
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="password"
+            className="text-lg text-sky-900 font-semibold"
+          >
+            Asignar Password al Archivo:
+          </label>
+          <input type="checkbox" onChange={()=>setAsignarPass(!asignarPass)} />
+        </div>
+        {asignarPass ? (
+          <input
+            type="password"
+            className="appearance-none w-full text-center mt-2 bg-white border-2 font-semibold border-rose-900 text-black py3 focus:outline-none rounded-md"
+          />
+        ) : null}
       </div>
     </div>
   );

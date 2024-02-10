60f2df511b3ff8138a16173b56f4ff50cb9911cc
Renier Vargas <renier1989@gmail.com>
2024-02-09 22:27:22 -0500

    allow http request

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+        {/* <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link

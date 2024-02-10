0d2c7d9d63dd551444bbd5a0f8d490b0a0c4e035
Renier Vargas <renier1989@gmail.com>
2024-02-09 22:16:31 -0500

    up fix port backend http

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        backendURL: '3.81.20.62:4001',
+        backendURL: 'http://3.81.20.62:4001',
         frontendURL: 'https://rushdrop-client-renier1989.vercel.app'
     }
 }
